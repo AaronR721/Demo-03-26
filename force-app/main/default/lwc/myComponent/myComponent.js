d08b1aff130d90d3cf0157c0dd169758726e51c1
Aaron Ruder <aaronruder721@gmail.com>
2026-03-18 21:28:42 -0700

    Js Change Late Night

--- force-app/main/default/lwc/myComponent/myComponent.js
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -3,6 +3,14 @@ import { LightningElement, api, track } from 'lwc';
 export default class MyComponent extends LightningElement {
     @api myVariable = '';
 
+    @track myVariable = '';
+
+
+
+    connectedCallback() {
+        this.myVariable = this.myVariable || '';
+    } 
+
     handleChange(event) {
         this.myVariable = event.target.value;
     }
